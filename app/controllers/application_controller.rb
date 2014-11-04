fdf4722b9e6e651443e33881891b25691f74189e
vanessadean <vanessa.dean@gmail.com>
2014-11-03 19:43:56 -0500

    Add flatiron logo to img

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,10 @@ require_relative "../../config/environment"
 class ApplicationController < Sinatra::Application
 
   get '/tweets' do
-    # Tweet.new("Vanessa", "My first tweet! SO EXCITING OMG!!!")
-    # Tweet.new("Vanessa", "My second tweet! Still super exciting!!!")
-    # Tweet.new("Vanessa", "A third tweet! ZOMG!!!")
+    Tweet.new("Vanessa", "My first tweet! SO EXCITING OMG!!!")
+    Tweet.new("Vanessa", "My second tweet! Still super exciting!!!")
+    Tweet.new("Vanessa", "A third tweet! ZOMG!!!")
+    Tweet.new("Vanessa", "This is so cool!")
     @tweets = Tweet.all
     erb :tweets
   end
