2ccc0449e1344f355d0006460945c065e0fe2c2e
vanessadean <vanessa.dean@gmail.com>
2015-04-08 16:06:44 -0400

    change to / and index instead of tweets

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,16 +9,12 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
-    "Welcome to Fwitter"
-  end
-
-  get '/tweets' do
     Tweet.new("Vanessa", "My first tweet! SO EXCITING OMG!!!")
     Tweet.new("Vanessa", "My second tweet! Still super exciting!!!")
     Tweet.new("Vanessa", "A third tweet! ZOMG!!!")
     Tweet.new("Vanessa", "This is so cool!")
     @tweets = Tweet.all
-    erb :tweets
+    erb :index
   end
-  
+
 end
