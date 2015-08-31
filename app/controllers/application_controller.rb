39871cd0aa1784f4f496b53b17de8fd38c8d4141
Ian Candy <iancandysemail@gmail.com>
2015-08-31 13:45:08 -0400

    add comments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,28 @@
+# Our controller's job is to take information from the models and pass it into the views. 
+
+#First, we load up all of the code in the environment file and tweet model. 
+
 require './config/environment'
 require './app/models/tweet'
 
 class ApplicationController < Sinatra::Base
 
   configure do
+    # this configure block sets some defaults for our application - this lets Sinatra know to look in our public folder for stylesheets and images, and our views for erb files. 
     set :public_folder, 'public'
     set :views, 'app/views'
   end
 
-  get '/' do
+  # the block below defines what will happen when a user goes to our home page
+  get '/' do 
+    # First, we hard code some new instances of our tweet class. Remember, these will get saved temporarily by the ALL_TWEETS array. 
     Tweet.new("Vanessa", "My first tweet! SO EXCITING OMG!!!")
     Tweet.new("Vanessa", "My second tweet! Still super exciting!!!")
     Tweet.new("Vanessa", "A third tweet! ZOMG!!!")
     Tweet.new("Vanessa", "This is so cool!")
+    # Using the all method that we created, we can store all of the tweets into an instance variable. 
     @tweets = Tweet.all
-    erb :index
+    erb :index #this will display whatever is in the file called index.erb in the 'app/views' directory
   end
 
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,12 +1,14 @@
+# Our models represent the logic of our application. Right now, we have a class of Tweet to represent our posts.
 class Tweet
-  ALL_TWEETS = []
+  ALL_TWEETS = [] #our tweet class has a variable called 'ALL_TWEETS'. This variable belongs to the whole class, not instances of the class. 
 
-  def initialize(username, status)
+  def initialize(username, status) #Each tweet will initialize with a username and a status. 
     @username = username
     @status = status
-    ALL_TWEETS << self
+    ALL_TWEETS << self #Every time a new tweet is created, it's added into the ALL_TWEETS array. In the initialize method, self refers to the tweet that was newly created. 
   end
 
+  # Here we have reader methods for our username and status. 
   def username
     @username
   end
@@ -15,7 +17,10 @@ class Tweet
     @status
   end
 
-  def self.all
+
+# Here, self indicates that this method will be called on the class of Tweet, not on the new instances on tweets. Example: @tweets = Tweet.all will make an instance variable called @tweets and set it equal to the ALL_TWEETS array
+
+  def self.all 
     ALL_TWEETS
   end
 end
