fc87458da1f9a0446c5f005a65eb7e4c3bf6d6d9
vanessaa0 <vanessa.castro.simontech.2016@gmail.com>
2016-03-15 22:14:04 +0000

    fwitter 2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,5 +12,14 @@ class ApplicationController < Sinatra::Base
     @tweets = Tweet.all
     erb :index 
   end
-
+  
+  get '/tweet' do
+    erb :tweet
+  end
+ 
+  post '/tweet' do
+    Tweet.new(params[:username], params[:status])
+    redirect '/'
+  end
+  
 end
